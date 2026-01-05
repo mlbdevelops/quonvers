afcce4a0071620a8110ba64fee9c2178be6d0d0c
Mlb L'original <loriginalmlb@gmail.com>
2026-01-05 13:08:47 +0200

    lolly4

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mlbdev.quonvers',
   appName: 'Quonvers',
-  webDir: '.next/standalone'
+  webDir: 'out'
 };
 
 export default config;
